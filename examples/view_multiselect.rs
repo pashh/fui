feaeeccced0f58eb4e92e024f00a367d77ca16dc
Tymoteusz Jankowski <tymoteusz.jankowski@gmail.com>
2018-02-12 17:07:41 +0100

    Add submit_anything to view Multiselect

--- examples/view_multiselect.rs
+++ examples/view_multiselect.rs
@@ -21,15 +21,21 @@ fn handler(_c: &mut Cursive, kind: &str, value: Rc<String>) {
 fn main() {
     let mut c = Cursive::new();
 
-    let widget = LinearLayout::vertical().child(
-        Multiselect::new(DirItems::new())
+    let widget = LinearLayout::vertical()
+        .child(
+            Multiselect::new(DirItems::new())
+                .on_select(|c, text| handler(c, "on_select", text))
+                .on_deselect(|c, text| handler(c, "on_deselect", text)),
+        )
+        .child(
+            Multiselect::new(DirItems::new())
                 // allows user to select single item many items
                 //TODO: .redundant_selection()
                 // allows user to select single out of completition
-                //TODO: .select_anything(handler)
+                .select_anything()
                 .on_select(|c, text| handler(c, "on_select", text))
                 .on_deselect(|c, text| handler(c, "on_deselect", text)),
-    );
+        );
 
     c.add_layer(widget);
 
